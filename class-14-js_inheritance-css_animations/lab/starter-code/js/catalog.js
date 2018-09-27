373e30f8b91c7fbdebb85ffe1311ac64f47b1340
Scott Currie <vitriolik@gmail.com>
2018-09-27 13:35:14 -0700

    added code to populate select

--- class-14-js_inheritance-css_animations/lab/starter-code/js/catalog.js
+++ class-14-js_inheritance-css_animations/lab/starter-code/js/catalog.js
@@ -12,7 +12,10 @@ function populateForm() {
   //TODO: Add an <option> tag inside the form's select for each product
   var selectElement = document.getElementById('items');
   for (var i in Product.allProducts) {
-
+    var option = document.createElement('option');
+    option.value = Product.allProducts[i].name;
+    option.innerHTML = Product.allProducts[i].name;
+    selectElement.appendChild(option);
   }
 
 }
